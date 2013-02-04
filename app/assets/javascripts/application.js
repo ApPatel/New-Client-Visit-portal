dcb3e4b6ce72c4fa2c59c3fbd3f74709fbbab305
nikita singh <nikitaa_singh@yahoo.co.in>
2013-02-04 14:24:00 +0530

    deleted some js files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,13 +13,63 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-//= require app
-//= require jquery.colorbox
-//= require jquery-ui-1.9.2.custom.min
-//= require dhtmlxcommon
-//= require dhtmlxgrid
-//= require dhtmlxgridcell
-//= require dhtmlxcalendar
-//= require dhtmlxgrid_excell_dhxcalendar
-//= require dhtmlxgrid_export.js
 
+
+
+
+$(function(){
+    if($("#devise_notice").text() != ""){
+        createDialog("<h3 class='heading3 alert-message'>"+ $("#devise_notice").text() +"</h3>");
+        $("#devise_notice").remove();
+    }
+    if($("#devise_alert").text() != ""){
+        createDialog("<h3 class='heading3 alert-message'>"+ $("#devise_alert").text() +"</h3>");
+        $("#devise_alert").remove();
+    }
+    function createDialog( message) {
+        $.colorbox({
+            html : message,
+            transition: 'fade',
+            width: 600,
+            height: 130,
+            scrolling: false
+        });
+        setTimeout(function() {
+            $.colorbox.close();
+        }, 2000);
+    }
+
+});
+
+
+
+
+$(function() {
+
+    $( "#visit_Date" ).datepicker({
+        showOn: "button",
+        buttonImage: "/assets/calendar_icon.jpg",
+        buttonImageOnly: true,
+        changeMonth: true,
+        changeYear: true,
+        numberOfMonths: 1,
+        showAnim: 'fold',
+        dateFormat: $.datepicker.ISO_8601
+        
+    });
+
+});
+
+
+
+
+$(function() {
+  $("#visits thead, #visits .pagination").on("click", "a", function() {
+    $.getScript(this.href);
+    return false;
+  });
+  $("#visits_search input").keyup(function() {
+    $.get($("#visits_search").attr("action"), $("#visits_search").serialize(), null, "script");
+    return false;
+  });
+});
