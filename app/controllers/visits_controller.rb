622cc33715c57d70cc3ff31f39985694f8b338e5
nikita singh <nikitaa_singh@yahoo.co.in>
2013-02-04 18:37:19 +0530

    bootstrap

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -43,7 +43,9 @@ class VisitsController < ApplicationController
                 agenda1.StartTimeMin = startmin.to_i
                 agenda1.EndTimeHour = endhour.to_i
                 agenda1.EndTimeMin = endmin.to_i
-                 if (agenda1.EndTimeMin>=agenda1.StartTimeMin) 
+                if (agenda1.EndTimeHour < agenda1.StartTimeHour)
+                 agenda1.timeduration="Invalid time duration"
+                 elsif (agenda1.EndTimeMin>=agenda1.StartTimeMin) 
 
                   agenda1.timeduration="#{(agenda1.EndTimeHour-agenda1.StartTimeHour)}"+" hr(s)"+" #{(agenda1.EndTimeMin-agenda1.StartTimeMin)}"+" mins"
 
@@ -52,6 +54,8 @@ class VisitsController < ApplicationController
 
                 elsif (agenda1.EndTimeMin==0 && agenda1.StartTimeMin!=0)
                   agenda1.timeduration="#{(agenda1.EndTime-agenda1.StartTime)-1}"+" hr(s)"+" #{(60-agenda1.StartTimeMin)}"+" mins"
+                    elsif (agenda1.EndTimeHour < agenda1.StartTimeHour)
+                 agenda1.timeduration="Invalid time duration"
                 end
                 agenda1.Agenda =agenda
                 agenda1.Details =details
@@ -73,7 +77,10 @@ class VisitsController < ApplicationController
                 agenda1.StartTimeMin = startmin.to_i
                 agenda1.EndTimeHour = endhour.to_i
                 agenda1.EndTimeMin = endmin.to_i
-                if (agenda1.EndTimeMin>=agenda1.StartTimeMin) 
+                if (agenda1.EndTimeHour < agenda1.StartTimeHour)
+               
+                 agenda1.timeduration="Invalid time duration"
+                elsif (agenda1.EndTimeMin>=agenda1.StartTimeMin) 
 
                   agenda1.timeduration="#{(agenda1.EndTimeHour-agenda1.StartTimeHour)}"+" hr(s)"+" #{(agenda1.EndTimeMin-agenda1.StartTimeMin)}"+" mins"
 
@@ -82,6 +89,8 @@ class VisitsController < ApplicationController
 
                 elsif (agenda1.EndTimeMin==0 && agenda1.StartTimeMin!=0)
                   agenda1.timeduration="#{(agenda1.EndTimeHour-agenda1.StartTimeHour)-1}"+" hr(s)"+" #{(60-agenda1.StartTimeMin)}"+" mins"
+                elsif (agenda1.EndTimeHour < agenda1.StartTimeHour)
+                 agenda1.timeduration="Invalid time duration"
                 end
                 agenda1.Agenda =agenda
                 agenda1.Details =details
